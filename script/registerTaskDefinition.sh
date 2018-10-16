f7febfab18d148ced5f94179f463af0ca7aba6a0
hhyhz <39365426+hhyhz@users.noreply.github.com>
2018-10-16 14:39:00 +0800

    Update registerTaskDefinition.sh

--- script/registerTaskDefinition.sh
+++ script/registerTaskDefinition.sh
@@ -11,8 +11,8 @@ file_task=$cur_dir/script/taskdefinition.json
 
 sed -i 's/\("family": "\).*/\1'"$taskFamily"'",/g' $file_task
 sed -i 's/\("name": "\).*/\1'"$projectName"'",/g' $file_task
-sed -i 's/\("containerPort": "\).*/\1'"$containerPort"'",/g' $file_task
-sed -i 's/\("hostPort": "\).*/\1'"$containerPort"'",/g' $file_task
+sed -i 's/\("containerPort": \).*/\1'"$containerPort"',/g' $file_task
+sed -i 's/\("hostPort": \).*/\1'"$containerPort"',/g' $file_task
 sed -i 's/\("cpu": "\).*/\1'"$customCpu"'",/g' $file_task
 sed -i 's/\("memory": "\).*/\1'"$customMemory"'"/g' $file_task
 sed -i 's/\("image": "\).*/\1'"${registryUrl}\/${projectName}"'",/g' $file_task

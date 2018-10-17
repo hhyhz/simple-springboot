c7000ca8ca0f4940072429542f2fc05231a3d05b
hhyhz <39365426+hhyhz@users.noreply.github.com>
2018-10-17 10:37:23 +0800

    Update createOrUpdateService.sh

--- script/createOrUpdateService.sh
+++ script/createOrUpdateService.sh
@@ -14,12 +14,12 @@ echo "targetGroup:${targetGroupArn}"
 
 aws elbv2 create-listener --load-balancer-arn ${loadBalancerArn} --protocol HTTP --port ${containerPort} --default-actions Type=forward,TargetGroupArn=${targetGroupArn}
 
-if aws ecs describe-services --service ${serviceName} --cluster ${clusterName} | grep "MISSING"
+if aws ecs describe-services --service ${serviceName} --cluster ${clusterName} | grep "INACTIVE"
 then
   aws ecs create-service --cluster ${clusterName} --service-name ${serviceName} --task-definition ${taskFamily} --desired-count ${desiredCount} --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[${subnet1},${subnet2}],securityGroups=[${securityGroup}],assignPublicIp=ENABLED}" --load-balancers targetGroupArn=${targetGroupArn},containerName=${projectName},containerPort=${containerPort}
 elif aws ecs describe-services --service ${serviceName} --cluster ${clusterName} | grep "MISSING"
 then
   aws ecs create-service --cluster ${clusterName} --service-name ${serviceName} --task-definition ${taskFamily} --desired-count ${desiredCount} --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[${subnet1},${subnet2}],securityGroups=[${securityGroup}],assignPublicIp=ENABLED}" --load-balancers targetGroupArn=${targetGroupArn},containerName=${projectName},containerPort=${containerPort}
 else
-  aws ecs update-service --service ${serviceName} --task-definition ${taskFamily} --cluster ${clusterName}
+  aws ecs update-service --service ${serviceName} --task-definition ${taskFamily} --cluster ${clusterName} --desired-count ${desiredCount}
 fi
